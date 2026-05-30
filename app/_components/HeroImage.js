1f30f1a26980dba81f4cdf98807ce5171edb0c21
asanda-ngcobo <asanda.ngcobo@outlook.com>
2026-05-30 13:20:27 +0200

    radius edit

--- app/_components/HeroImage.js
+++ app/_components/HeroImage.js
@@ -6,7 +6,8 @@ function HeroImage() {
     return (
         <div className="w-[90%] mx-auto h-full md:w-[50%]
      md:mx-0 md:h-[72vh]  md:flex
-     items-center justify-center rounded-bl-3xl bg-(--accent-secondary)">
+     items-center justify-center rounded-3xl md:rounded-bl-3xl 
+     md:rounded-tl-3xl md:rounded-br-none rounded-tr-none   bg-(--accent-secondary)">
             <Image src={Hero} alt="Preci ai Summary" width={800}
             className="rounded-2xl  mx-auto"/>
         </div>
