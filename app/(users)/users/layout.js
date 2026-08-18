47b9aa739de2cef4935487c4b21e8bde9477f183
Asanda Excellent Ngcobo <105105413+Asanda-Ngcobo@users.noreply.github.com>
2026-08-18 20:54:42 +0200

    Update layout.js

--- app/(users)/users/layout.js
+++ app/(users)/users/layout.js
@@ -89,7 +89,10 @@ export default function RootLayout({ children }) {
         <MenuProvider>
          {children}
         </MenuProvider>
-        
+           <Script
+          src="https://js.paystack.co/v1/inline.js"
+          strategy="afterInteractive"
+        />
       </body>
 
   </html>
