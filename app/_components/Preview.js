ca30b07e222e70d69fd35a7df98257bc586d8b43
Asanda Excellent Ngcobo <105105413+Asanda-Ngcobo@users.noreply.github.com>
2026-08-22 14:12:14 +0200

    Update Preview.js

--- app/_components/Preview.js
+++ app/_components/Preview.js
@@ -102,12 +102,12 @@ router.push(`/summary/${summaryId}/checkout?token=${token}`);
 
       <div className="mt-4 flex justify-between w-[60%]">
         <span className="text-gray-500">Full Summary</span>
-        <span className="font-medium">
+  {/* <span className="font-medium">
           {displayPrice}
           {discountActive && (
             <span className="ml-2 text-xs text-green-600 font-normal">67% off</span>
           )}
-        </span>
+        </span> */}
       </div>
 
       <button
