b6bf98ad40546f73088dfc74851092235278d945
Asanda Excellent Ngcobo <105105413+Asanda-Ngcobo@users.noreply.github.com>
2026-08-30 13:37:59 +0200

    Update UploadOptions.js

--- app/_components/UploadOptions.js
+++ app/_components/UploadOptions.js
@@ -4,7 +4,7 @@ import { useState, useRef } from "react";
 import { Upload, FileText, X } from "@deemlol/next-icons";
 import { useMenu } from "../providers/MenuProvider";
 
-function UploadOptions() {
+function UploadOptions({handleProcess}) {
  const {file, setFile} = useMenu();
   const [isDragging, setIsDragging] = useState(false);
   const inputRef = useRef(null);
@@ -35,7 +35,8 @@ function UploadOptions() {
 
   return (
     <>
-    {!file && <div className="w-40 -mx-5 max-w-sm absolute -mt-45 z-20">
+    <div className="w-full min-h-screen flex flex-col gap-8 justify-center items-center backdrop-blur-2xl fixed z-20 top-0 left-0">
+    {!file && <div className="w-40 -mx-5 max-w-sm absolute -mt-45 z-30">
       <div
         onClick={() => inputRef.current.click()}
         onDragOver={(e) => {
@@ -74,21 +75,42 @@ function UploadOptions() {
     </div>} 
       {/* File Preview */}
       {file && (
-        <div className="max-w-80 md:max-w-[90%]  flex items-center justify-between rounded-xl bg-gray-50 px-4 py-5 mx-auto">
+        <div className="max-w-80 md:max-w-[90%] 
+        h-60 flex flex-col gap-2 items-center justify-between rounded-xl bg-gray-50
+         px-4 py-5 mx-auto">
+           <button
+            onClick={() => setFile(null)}
+            className="text-gray-400 hover:text-gray-600"
+          >
+            <X size={16} />
+          </button>
+
           <div className="flex items-center gap-2">
             <FileText size={18} className="text-gray-500" />
             <span className="text-xs text-gray-700 truncate max-w-60  md:max-w-full">
               {file.name}
             </span>
           </div>
-          <button
-            onClick={() => setFile(null)}
-            className="text-gray-400 hover:text-gray-600"
-          >
-            <X size={16} />
-          </button>
+         
         </div>
-      )}</>
+      )}
+
+      {file &&   <button
+  disabled={!file}
+  onClick={handleProcess}
+    className={`
+       flex items-center justify-center
+      h-8 w-fit py-2 px-5 rounded-xl
+      cursor-pointer
+  
+       
+      hover:bg-(--accent-secondary)  font-bold text-amber-50 ${file ? 'bg-(--accent-primary)': 'bg-(--accent-secondary)'} `}
+     
+    
+  >
+    Analyize
+  </button>}
+      </div></>
    
   );
 }
